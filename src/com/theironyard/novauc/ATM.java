3001c21b909fe4b7c1baee6c2933f965e7d71e7e
Steve Jobs <peter@Steves-MBP.home>
2017-02-11 17:44:26 -0500

    have a hashmap, and just stuck

--- src/com/theironyard/novauc/ATM.java
+++ src/com/theironyard/novauc/ATM.java
@@ -3,78 +3,49 @@ package com.theironyard.novauc;
 import java.util.HashMap;
 import java.util.Scanner;
 
+
 public class ATM {
 
-    public static Scanner scanner = new Scanner(System.in);
-    public static Scanner scanner2 = new Scanner(System.in);
-    public static User users = new User();
+    Scanner input = new Scanner(System.in);
 
 
     public static void main(String[] args) throws Exception {
-        // write your code here
 
-        System.out.println("Let's get crazy");
+        HashMap<String, User> switchBoard = new HashMap<>();
+        switchBoard.put("Gmoney", new User("Washington", 55.55));
+        switchBoard.put("Bfreezy", new User("Clinton", 77.77));
 
-        //users.getPeeps().put("peter", 9.99);
+        //testing a few things to prove that I can get the information from the hashmap
+        String informationName = switchBoard.get("Gmoney").getName();
+        System.out.println(informationName);
 
+        double informationBalance = switchBoard.get("Gmoney").getBalance();
+        System.out.println(informationBalance);
 
-        users.askName();
-        users.nameTest();
+        String informationName2 = switchBoard.get("Bfreezy").getName();
+        System.out.println(informationName2);
 
-//        HashMap<String, Double> peeps = new HashMap();
-//       peeps.put("Peter",99.99);
-//        peeps.put("Abraham",88.88);
+        double informationBalance2 = switchBoard.get("Bfreezy").getBalance();
+        System.out.println(informationBalance2);
 
+        boolean informationName3 = switchBoard.containsKey("Gmoney");
+        System.out.println(informationName3);
 
-        //Boolean peepsNames = peeps.containsKey(users.theUsersNameIs);
+        //testing to make sure that i can elicit a false response against the hashmap
+        boolean informationBalance3 = switchBoard.containsKey("TooSmooth");
+        System.out.println(informationBalance3);
 
+        //trying to call a method that will take the user's input [name], check it against the hashmap,
+        //and return a boolean value
+        User.informationBalance4();
 
-
-        System.out.println("Boom-shaka-laka!");
-    }
-  }
-
-
-/*
-
-
-
-
-
-
-
-
-
+        boolean informationBalance5 = switchBoard.containsKey("Gmoney");
+        User.enteredName();
+        System.out.println("Welcome +" enteredName);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        HashMap<String, Double> peeps = new HashMap();
-        peeps.put("Peter",99.99);
-        peeps.put("Abraham",88.88);
-        //peeps.put(pullname.fromsomwhere,pullcorresponding.balance)
-
-        //probably something with a scanner
-        //System.out.println("Want to see a balance?");
-
-        Boolean peepsNames = peeps.containsKey(users.theUsersNameIs);
-        //either a scanner here, or in the above switch to handle an input?
-        System.out.println(peepsNames);
-        System.out.println()
-        //prints boolean peepsNames value, but exits through nameTest
-        users.nameTest();
     }
 }
-*/
+
+
--- src/com/theironyard/novauc/User.java
+++ src/com/theironyard/novauc/User.java
@@ -1,46 +1,34 @@
 package com.theironyard.novauc;
 
-import java.util.HashMap;
+
 
 /**
  * Created by peter on 2/9/17.
  */
 public class User {
 
-    String theUsersNameIs;
-    String nameTest;
-    Boolean peepsNames;
-    String peeps1;
-    HashMap<String, Double> peeps = new HashMap<>();
+    String nameSwitchBoard;
+    double balanceSwtichBoard;
+    int key;
+    String enteredName;
 
-//      HashMap<String, Double> peepsy = new HashMap();
-//       peeps.put("Peter",99.99);
-//        peeps.put("Abraham",88.88);
-
-
-    public HashMap<String, Double> getPeeps() {
-        return peeps;
+    User(String nameSwitchBoard, double balanceSwtichBoard) {
+        this.nameSwitchBoard = nameSwitchBoard;
+        this.balanceSwtichBoard = balanceSwtichBoard;
     }
 
-    public void setPeeps(HashMap<String, Double> peeps) {
-        this.peeps = peeps;
+    String getName() {
+        return nameSwitchBoard;
     }
-
-    public void askName() throws Exception {
-        System.out.println("What is your name?");
-        theUsersNameIs = ATM.scanner.nextLine();
-        System.out.println("Welcome, " + theUsersNameIs);
-
+    double getBalance() {
+        return balanceSwtichBoard;
     }
-
-    public void nameTest() throws Exception {
-        System.out.println("made it to nameTest");
-        if (peepsNames == true) {
-            System.out.println("the name entered is truly true!");
-        }
-        else {
-            System.out.println("intruder!");
-        }
+    public void informationBalance4 throws Exception {
+        System.out.println("Showing User class informationBalance4");
+    }
+    boolean informationBalance4 = switchBoard.containsKey("Gmoney") {
+        enteredName = ATM.input.nextLine();
+        System.out.println("Welcome, " + enteredName);
     }
 
 }
@@ -54,168 +42,18 @@ public class User {
 
 
 
-        /*
 
 
 
 
 
 
-    String option;
 
 
 
-    }
-//    public void chooseName() throws Exception {
-//        System.out.println("What is your name?");
-//        name = ATM.scanner.nextLine();
-//        System.out.println("Welcome, " + name);
-//    }
-
-
-    public void nameTest() throws Exception {
-        //takes boolean value of peepsNames and run an if - hopefully a switch
-        System.out.println("Made it this far");
-        //exit's after this point
-        if (peepsNames == true) {
-            System.out.println("A genius must have written this");
-        }
-        else {
-            System.out.println("Not looking good! :) :) :)");
-        }
-
-    }
-
-    //public User() {}
-
-    public void chooseOption() throws Exception {
-        int loopdiloop = 1;
-        while(loopdiloop == 1) {
-            System.out.println("What would you like to do? [Check balance / Withdraw funds / Cancel my life]");
-            option = ATM.scanner.nextLine();
-            if (option.equalsIgnoreCase("Check balance")) {
-                System.out.println("Current balance: " + this.balance);
-            }
-
-            else if (option.equalsIgnoreCase("Withdraw funds")) {
-                System.out.println("How much?");
-                double moneyRequested = ATM.scanner2.nextDouble();
-                if (this.balance >= moneyRequested) {
-                    System.out.println("Printing " + moneyRequested);
-                    this.balance = this.balance - moneyRequested;
-                    System.out.println("Remaining balance: " + this.balance);
-                } else {
-                    System.out.println("Does not compute");
-                }
-            }
-
-            else if (option.equalsIgnoreCase("Cancel my life")) {
-                System.out.println("Good choice");
-                loopdiloop = 0;
-
-
-            } else {
-                throw new Exception("Invalid balance statement");
-            }
-        }
-
-    }
-}
 
 
 
-    /*
-    //MENUS
-        //Current
-        //New User
-        //New User Prompts
-        //Current user Prompts
-
-    String OuterMostMenu;
-    String OuterMostMenu2;
-    
-    //first level existing or new user?
-
-    public void chooseOuterMostMenu() throws Exception {
-        int loopdiloop1 = 1;
-        while (loopdiloop1 == 1) {
-            System.out.println("existing user or new user? [Current User / New User]");
-
-            OuterMostMenu2 = ATM.scanner.nextLine();
-            if (OuterMostMenu2.equalsIgnoreCase("Current User?")) {
-                System.out.println("All up in the heezy!");
-            }
-            else if (OuterMostMenu2.equalsIgnoreCase("New User?")) {
-                System.out.println("idk");
-            }
-            else if (OuterMostMenu2.equalsIgnoreCase("Current User Options")) {
-                System.out.println("idk");
-            }
-            else if (OuterMostMenu2.equalsIgnoreCase("New User Prompts")) {
-                loopdiloop1 = 0;
-            }
-            else {
-                throw new Exception("Does not compute at first level");
-            }
-        }
-    }
-
-
-    //second level existing user true
-    //public void
-
-    /*
-            switch () {
-                case
-            })
-
-        }
-    }
-    //What is your name - if true then x, if false then y
-    //name_true - current user
-    //name_false - not current user - "would you like to create an account?" yes/no
-
-    //current user - four OuterMostMenu2s
-    //checkBalance;
-    //manageFunds; //withdraw //transfer //add
-    //manageProfile; //meh do it later!
-    //cancel;
-
-    /*
-    //current user? yes/no
-    //user is asked to put in their card [card] or create profile
-    //if [card] then account
-    //if create profile then profile
-
-    //current user yes
-    String profile; //currentAccount //newAccount
-    String withdraw;
-    String cancel;
-    double balance;
-    double transfer;
-    String anotherProfile;
-    String deleteProfile;
-
-    //current user no
-    String nameFirst;
-    String nameLast;
-    int pin;
-    int phoneNum;
-    String moneyIn;
-
-    //current user yes, another
-    //int pin;
-    //string moneyIn;
-    //double transfer
-
-    //inside withdraw
-    String howMuch;
-
-    //inside cancel
-    String enjoy;
-
-    //inside balance
-    */
 
 
 
