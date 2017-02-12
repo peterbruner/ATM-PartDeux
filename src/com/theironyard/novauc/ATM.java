99255ddbb7329203435ba47f9b5d19dc6038b118
Steve Jobs <peter@Steves-MacBook-Pro.local>
2017-02-12 17:26:06 -0500

    significant progress

--- src/com/theironyard/novauc/ATM.java
+++ src/com/theironyard/novauc/ATM.java
@@ -1,49 +1,14 @@
 package com.theironyard.novauc;
 
-import java.util.HashMap;
-import java.util.Scanner;
-
 
 public class ATM {
-
-    Scanner input = new Scanner(System.in);
+    public static User user = new User();
 
 
     public static void main(String[] args) throws Exception {
-
-        //HashMap<String, Double> switchBoard = new HashMap<>();
-
-        HashMap<String, User> switchBoard = new HashMap<>();
-        switchBoard.put("Gmoney", new User("Washington", 55.55));
-        switchBoard.put("Bfreezy", new User("Clinton", 77.77));
-
-        //testing a few things to prove that I can get the information from the hashmap
-        String informationName = switchBoard.get("Gmoney").getName();
-        System.out.println(informationName);
-
-        double informationBalance = switchBoard.get("Gmoney").getBalance();
-        System.out.println(informationBalance);
-
-        String informationName2 = switchBoard.get("Bfreezy").getName();
-        System.out.println(informationName2);
-
-        double informationBalance2 = switchBoard.get("Bfreezy").getBalance();
-        System.out.println(informationBalance2);
-
-        boolean informationName3 = switchBoard.containsKey("Gmoney");
-        System.out.println(informationName3);
-
-        //testing to make sure that i can elicit a false response against the hashmap
-        boolean informationBalance3 = switchBoard.containsKey("TooSmooth");
-        System.out.println(informationBalance3);
-
-        //trying to call a method that will take the user's input [name], check it against the hashmap,
-        //and return a boolean value
-        //User.informationBalance4();
-
-//        boolean informationBalance5 = switchBoard.containsKey("Gmoney");
-//        User.enteredName();
-//        System.out.println("Welcome +" enteredName);
+       user.login();
+       user.menu();
+       user.addUser();
 
 
 
--- src/com/theironyard/novauc/User.java
+++ src/com/theironyard/novauc/User.java
@@ -1,41 +1,99 @@
 package com.theironyard.novauc;
 
 
+import java.util.Scanner;
+import java.util.HashMap;
 
 /**
  * Created by peter on 2/9/17.
  */
 public class User {
+    Scanner input = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    HashMap<String, Double> switchBoard = new HashMap<>();
+    String name;
+    double deposit;
+    int loopdiloop = 1;
 
-    String nameSwitchBoard;
-    double balanceSwtichBoard;
-    int key;
-    String enteredName;
 
-    User(String nameSwitchBoard, double balanceSwtichBoard) {
-        this.nameSwitchBoard = nameSwitchBoard;
-        this.balanceSwtichBoard = balanceSwtichBoard;
+
+    public void login() {
+        while (loopdiloop == 1) {
+            System.out.println("Welcome, what is your name? Type exit to quit.");
+            name = input.nextLine().toUpperCase();
+
+            if (switchBoard.containsKey(name)) {
+                menu();
+            }
+            else if(name.equals("EXIT")) {
+                //TODO: Possible to add method
+                System.exit(0);
+            }
+            else {
+                //System.out.println("this line is addUser");
+                addUser();
+            }
+        }
     }
 
-    String getName() {
-        return nameSwitchBoard;
+    public void menu(){
+        while (loopdiloop == 1) {
+            System.out.println("What would you like to do?");
+            System.out.println("withdraw / check balance / remove account / cancel");
+            switch (input.nextLine()) {
+                case "withdraw":
+                                    //System.out.println("this is withdraw");
+                                    withdraw();
+                                    break;
+                case "check balance":
+                                        //System.out.println("this is checkBalance");
+                                        //checkBalance();
+                                        break;
+                case "remove account":
+                                        //System.out.println("this is removeAccount");
+                                        //removeAccount();
+                                        break;
+                case "cancel":
+                                //System.out.println("this is cancel");
+                                //cancel();
+                                return;
+                                //break;
+                default: System.out.println("Try again");
+            }
+
+        }
     }
 
-    double getBalance() {
-        return balanceSwtichBoard;
+    public void addUser() {
+        //TODO: grab the person's name and remove the below print
+        System.out.println("What is your first name?");
+        name = input.nextLine().toUpperCase();
+        System.out.println("Using digits only, how much money would you like to deposit?");
+        deposit = input2.nextDouble();
+        switchBoard.put(name, deposit);
+        System.out.println("Account created for " + name + " with " + deposit + " dollars");
+        menu();
     }
 
-    public void informationBalance4() {
-        System.out.println("Showing User class informationBalance4");
+    public void withdraw() {
+        double withdrawAmount;
+        while (loopdiloop == 1) {
+            System.out.println("How much would you like to withdraw? (Enter 0 to cancel)");
+
+            withdrawAmount = input2.nextDouble();
+            if (withdrawAmount <= switchBoard.get(name)) {
+                System.out.println("Withdrawing $" + withdrawAmount);
+                switchBoard.put(name, (switchBoard.get(name) - withdrawAmount));
+                System.out.println("balance $" + switchBoard.get(name));
+                return;
+            }
+            else {
+                System.out.println("You don't have that much");
+            }
+        }
     }
 
-    boolean informationBalance4(String name) {
-        //enteredName = ATM.input.nextLine();
-        //return, true or false or variable that evaluates to t/f
-        //void return type for boolean
-        System.out.println("Welcome, " + enteredName);
-        return true;
-    }
+
 
 }
 
